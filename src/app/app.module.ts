35b6f0b6b87d8be2a50bcbcb1d4fc4e2fe508f2d
Victorteka <ambukateka@gmail.com>
2019-11-02 19:29:17 +0300

    Add loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { NgProgressModule } from "@ngx-progressbar/core";
+import { NgProgressHttpClientModule } from "@ngx-progressbar/http-client";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -10,9 +12,9 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { SearchFormComponent } from "./components/search-form/search-form.component";
 import { RepoOverlayDirective } from "./repo-overlay.directive";
 import { DateCountPipe } from "./date-count.pipe";
-import { RepoComponent } from './components/repo/repo.component';
-import { SearchRepoComponent } from './components/search-repo/search-repo.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RepoComponent } from "./components/repo/repo.component";
+import { SearchRepoComponent } from "./components/search-repo/search-repo.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 
 @NgModule({
   declarations: [
@@ -26,7 +28,14 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     SearchRepoComponent,
     NotFoundComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
